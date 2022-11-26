2fe5def2f91b2898af97c149409162b53ac99501
GeorgeMichalakis <gmichalakis26@hotmail.com>
2022-11-26 15:52:02 +0100

    Change ₹

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -48,7 +48,7 @@ const CartScreen = ({ match, location, history }) => {
                   <Col md={3}>
                     <Link to={`/product/${item.product}`}>{item.name}</Link>
                   </Col>
-                  <Col md={2}>₹{item.price}</Col>
+                  <Col md={2}>${item.price}</Col>
                   <Col md={2}>
                     <Form.Control
                       type='text'
@@ -85,7 +85,7 @@ const CartScreen = ({ match, location, history }) => {
                 Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
                 Kgs
               </h2>
-              ₹
+              $
               {cartItems
                 .reduce((acc, item) => acc + item.qty * item.price, 0)
                 .toFixed(2)}
