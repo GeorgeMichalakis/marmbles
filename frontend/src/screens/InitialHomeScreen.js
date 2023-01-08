9d81950de620a05101e89da8eb85c322b6bdfaba
GeorgeMichalakis <gmichalakis26@hotmail.com>
2023-01-08 16:45:57 +0100

    test product

--- frontend/src/screens/InitialHomeScreen.js
+++ frontend/src/screens/InitialHomeScreen.js
@@ -36,7 +36,9 @@ const HomeScreen = ({ match }) => {
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
+                if (product.brand == "Block") {
+                    <Product product={product} />
+                  }
               </Col>
             ))}
           </Row>
